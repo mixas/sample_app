bd6cf7895723fc459fcfaf5ab4fe0ccc7f75e1a5
mixas <mixas.shel@gmail.om>
2011-09-06 18:55:56 +0300

    sample dinamic pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+    @title = "home"
+  end
+
+  def contact
+    @title = "contact"
+  end
+
+  def about
+    @title = "about"
+  end
+
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have right title" do
+      get 'home'
+      response.should have_selector("title", :content => " | home")
+    end
+
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have right title" do
+      get 'contact'
+      response.should have_selector("title", :content => " | contact")
+    end
+
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success  #answer from browser on 'get' request should be ... (200 Ok)
+    end
+
+    it "should have right title" do
+      get 'about'
+      response.should have_selector("title", :content => " | about")
+    end
+
+  end
+end
