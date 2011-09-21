450e60a22f603e502c90c72553c36f6f78a535f6
mixas <mixas.shel@gmail.om>
2011-09-21 17:51:03 +0300

    creating rest architerture for user resource

--- app/controllers/user_controller.rb
+++
@@ -1,6 +0,0 @@
-class UserController < ApplicationController
-  def new
-    @title = "new user"
-  end
-
-end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def new
+    @title = "new user"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
